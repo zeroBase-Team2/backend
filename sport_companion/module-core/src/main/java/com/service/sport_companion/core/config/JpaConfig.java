b6642ff9cc5fa32e8609ecb57d57666e3e91ab9f
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-11-18 01:30:54 +0900

    Feat : JpaConfig 클래스 구현

---
+++ sport_companion/module-core/src/main/java/com/service/sport_companion/core/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.service.sport_companion.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
