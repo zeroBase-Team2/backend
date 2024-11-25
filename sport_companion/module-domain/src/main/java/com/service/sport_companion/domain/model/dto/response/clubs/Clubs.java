78a8af5acc3b4e49091a80885d50fb430a0d1a98
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-11-26 02:12:49 +0900

    Feat : Clubs DTO 객체 구현

---
+++ sport_companion/module-domain/src/main/java/com/service/sport_companion/domain/model/dto/response/clubs/Clubs.java
@@ -0,0 +1,12 @@
+package com.service.sport_companion.domain.model.dto.response.clubs;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Clubs {
+
+  String clubName;
+  String emblemImg;
+}
