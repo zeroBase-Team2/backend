898b4c4fd3e90c3f90826b8a4e0acb180b9655dd
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-11-28 20:10:47 +0900

    Fix : AuthController 수정

---
+++ sport_companion/module-domain/src/main/java/com/service/sport_companion/domain/model/auth/KakaoCode.java
@@ -0,0 +1,12 @@
+package com.service.sport_companion.domain.model.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class KakaoCode {
+
+  private String code;
+
+}
