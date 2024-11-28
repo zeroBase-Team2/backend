ac756d8d6b6bc480ad0ea00b740ca2eaf2793c31
yeon <p-yo00@naver.com>
2024-11-28 23:39:23 +0900

    Test : PageResponse를 반환하도록 테스트 수정

--- sport_companion/module-api/src/test/java/com/service/sport_companion/api/service/impl/NewsServiceImplTest.java
+++ sport_companion/module-api/src/test/java/com/service/sport_companion/api/service/impl/NewsServiceImplTest.java
@@ -7,16 +7,17 @@ import static org.mockito.BDDMockito.given;
 
 import com.service.sport_companion.api.component.NewsHandler;
 import com.service.sport_companion.domain.model.dto.request.news.NewsParameterDto;
+import com.service.sport_companion.domain.model.dto.response.PageResponse;
 import com.service.sport_companion.domain.model.dto.response.ResultResponse;
 import com.service.sport_companion.domain.model.type.NewsType;
 import com.service.sport_companion.domain.model.type.SuccessResultType;
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 
@@ -35,13 +36,13 @@ class NewsServiceImplTest {
     // given
     given(newsHandler.findNewsByNewsDateOrderByRecent(
       any(NewsParameterDto.class), any(NewsType.class), any(Pageable.class))
-    ).willReturn(List.of());
+    ).willReturn(Page.empty());
 
     // when
     ResultResponse response = newsService.getTodayNews(new NewsParameterDto(), NewsType.TEXT, Pageable.ofSize(5));
 
     // then
-    assertInstanceOf(List.class, response.getData());
+    assertInstanceOf(PageResponse.class, response.getData());
     assertEquals(response.getStatus(), HttpStatus.OK);
     assertEquals(response.getMessage(), SuccessResultType.SUCCESS_GET_NEWS_LIST.getMessage());
   }
