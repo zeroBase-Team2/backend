a4432ee8c0821426c7eca863a8213ac0bf1f613c
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-11-28 21:43:42 +0900

    Refactor : DTO 클래스 Json 파싱 어노테이션 추가

--- sport_companion/module-domain/src/main/java/com/service/sport_companion/domain/model/auth/KakaoCode.java
+++ sport_companion/module-domain/src/main/java/com/service/sport_companion/domain/model/auth/KakaoCode.java
@@ -1,12 +1,17 @@
 package com.service.sport_companion.domain.model.auth;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class KakaoCode {
 
+  @JsonProperty("code")
   private String code;
 
+  @JsonCreator
+  public KakaoCode(String code) {
+    this.code = code;
+  }
 }
--- sport_companion/module-domain/src/main/java/com/service/sport_companion/domain/model/dto/request/auth/SignUpDto.java
+++ sport_companion/module-domain/src/main/java/com/service/sport_companion/domain/model/dto/request/auth/SignUpDto.java
@@ -1,14 +1,25 @@
 package com.service.sport_companion.domain.model.dto.request.auth;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class SignUpDto {
 
+  @JsonProperty("providerId")
   private String providerId;
+
+  @JsonProperty("nickname")
   private String nickname;
+
+  @JsonProperty("clubName")
   private String clubName;
 
+  @JsonCreator
+  public SignUpDto(String providerId, String nickname, String clubName) {
+    this.providerId = providerId;
+    this.nickname = nickname;
+    this.clubName = clubName;
+  }
 }
