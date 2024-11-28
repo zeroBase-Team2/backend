b7d5eedfb6bcfff013cd277520dcdd556b3c407a
yeon <p-yo00@naver.com>
2024-11-28 21:10:27 +0900

    Feat : Object 제너릭 타입으로 수정

--- sport_companion/module-domain/src/main/java/com/service/sport_companion/domain/model/dto/response/ResultResponse.java
+++ sport_companion/module-domain/src/main/java/com/service/sport_companion/domain/model/dto/response/ResultResponse.java
@@ -7,33 +7,33 @@ import org.springframework.http.HttpStatus;
 
 
 @Getter
-public class ResultResponse {
+public class ResultResponse<T> {
 
   private final Integer code;
   private final HttpStatus status;
   private final String message;
-  private final Object data;
+  private final T data;
 
-  public ResultResponse(SuccessResultType successResultCode, Object data) {
+  public ResultResponse(SuccessResultType successResultCode, T data) {
     this.code = successResultCode.getStatus().value();
     this.status = successResultCode.getStatus();
     this.message = successResultCode.getMessage();
     this.data = data;
   }
 
-  public ResultResponse(FailedResultType failedResultCode,  Object data) {
+  public ResultResponse(FailedResultType failedResultCode,  T data) {
     this.code = failedResultCode.getStatus().value();
     this.status = failedResultCode.getStatus();
     this.message = failedResultCode.getMessage();
     this.data = data;
   }
 
-  public static ResultResponse of(SuccessResultType successResultCode) {
-    return new ResultResponse(successResultCode, null);
+  public static ResultResponse<Void> of(SuccessResultType successResultCode) {
+    return new ResultResponse<>(successResultCode, null);
   }
 
-  public static ResultResponse of(FailedResultType failedResultCode) {
-    return new ResultResponse(failedResultCode, null);
+  public static ResultResponse<Void> of(FailedResultType failedResultCode) {
+    return new ResultResponse<>(failedResultCode, null);
   }
 
 }
