0eff56008cb19f29c400ca77088aae8f8842eaa2
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-12-01 17:49:14 +0900

    Feat : 데이터 조회 및 저장 Repository 메서드 추가

--- sport_companion/module-domain/src/main/java/com/service/sport_companion/domain/repository/SportRepository.java
+++ sport_companion/module-domain/src/main/java/com/service/sport_companion/domain/repository/SportRepository.java
@@ -1,10 +1,13 @@
 package com.service.sport_companion.domain.repository;
 
 import com.service.sport_companion.domain.entity.SportsEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SportRepository extends JpaRepository<SportsEntity, Long> {
+
+  Optional<SportsEntity> findBySportName(String sportName);
 
 }
