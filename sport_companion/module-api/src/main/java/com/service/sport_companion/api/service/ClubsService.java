19b8ca3f01dfdf87c7a4815fcfa1d4569ca2414b
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-12-01 17:48:04 +0900

    Feat : 구단 등록 API 구현

--- sport_companion/module-api/src/main/java/com/service/sport_companion/api/service/ClubsService.java
+++ sport_companion/module-api/src/main/java/com/service/sport_companion/api/service/ClubsService.java
@@ -1,10 +1,15 @@
 package com.service.sport_companion.api.service;
 
+import com.service.sport_companion.domain.model.dto.request.club.ClubDto;
 import com.service.sport_companion.domain.model.dto.response.ResultResponse;
 import com.service.sport_companion.domain.model.dto.response.clubs.Clubs;
+import java.io.IOException;
 import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface ClubsService {
 
   ResultResponse<List<Clubs>> getAllClubList();
+
+  ResultResponse<Void> addClub(ClubDto clubDto) throws IOException;
 }
--- sport_companion/module-api/src/main/java/com/service/sport_companion/api/service/impl/ClubsServiceImpl.java
+++ sport_companion/module-api/src/main/java/com/service/sport_companion/api/service/impl/ClubsServiceImpl.java
@@ -1,14 +1,23 @@
 package com.service.sport_companion.api.service.impl;
 
 import com.service.sport_companion.api.component.ClubsHandler;
+import com.service.sport_companion.api.component.ReservationSiteHandler;
+import com.service.sport_companion.api.component.S3Handler;
+import com.service.sport_companion.api.component.SportHandler;
 import com.service.sport_companion.api.service.ClubsService;
+import com.service.sport_companion.domain.entity.ClubsEntity;
+import com.service.sport_companion.domain.entity.ReservationSiteEntity;
+import com.service.sport_companion.domain.entity.SportsEntity;
+import com.service.sport_companion.domain.model.dto.request.club.ClubDto;
 import com.service.sport_companion.domain.model.dto.response.ResultResponse;
 import com.service.sport_companion.domain.model.dto.response.clubs.Clubs;
 import com.service.sport_companion.domain.model.type.SuccessResultType;
+import java.io.IOException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @Service
@@ -16,6 +25,10 @@ import org.springframework.stereotype.Service;
 public class ClubsServiceImpl implements ClubsService {
 
   private final ClubsHandler clubsHandler;
+  private final S3Handler s3Handler;
+  private final SportHandler sportHandler;
+  private final ReservationSiteHandler reservationSiteHandler;
+
 
   // 모든 구단 조회
   @Override
@@ -25,4 +38,23 @@ public class ClubsServiceImpl implements ClubsService {
 
     return new ResultResponse<>(SuccessResultType.SUCCESS_GET_ALL_CLUBS_LIST, clubList);
   }
+
+  @Override
+  public ResultResponse<Void> addClub(ClubDto clubDto) throws IOException {
+    String emblemImg = s3Handler.upload(clubDto.getFile());
+
+    SportsEntity sport = sportHandler.findBySportName(clubDto.getSportsName());
+    ReservationSiteEntity site = reservationSiteHandler.findBySiteName(clubDto.getSiteName());
+
+    clubsHandler.saveClub(ClubsEntity.builder()
+        .sports(sport)
+        .clubName(clubDto.getClubName())
+        .clubStadium(clubDto.getClubStadium())
+        .reservationSite(site)
+        .emblemImg(emblemImg)
+        .build());
+
+    return ResultResponse.of(SuccessResultType.SUCCESS_ADD_CLUB);
+  }
+
 }
