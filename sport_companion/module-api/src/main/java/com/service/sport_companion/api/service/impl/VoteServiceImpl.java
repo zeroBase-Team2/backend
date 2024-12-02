bd04973a48bb8f09236c25ef971628d193dc7d84
yeon <p-yo00@naver.com>
2024-12-02 15:47:04 +0900

    Fix : Config를 통해 관리자 권한 검증

--- sport_companion/module-api/src/main/java/com/service/sport_companion/api/service/impl/VoteServiceImpl.java
+++ sport_companion/module-api/src/main/java/com/service/sport_companion/api/service/impl/VoteServiceImpl.java
@@ -1,18 +1,15 @@
 package com.service.sport_companion.api.service.impl;
 
 import com.service.sport_companion.api.component.CandidateHandler;
-import com.service.sport_companion.api.component.UserHandler;
 import com.service.sport_companion.api.component.VoteHandler;
 import com.service.sport_companion.api.service.VoteService;
 import com.service.sport_companion.core.exception.GlobalException;
 import com.service.sport_companion.domain.entity.CandidateEntity;
-import com.service.sport_companion.domain.entity.UsersEntity;
 import com.service.sport_companion.domain.entity.VoteEntity;
 import com.service.sport_companion.domain.model.dto.request.vote.CreateVoteDto;
 import com.service.sport_companion.domain.model.dto.response.ResultResponse;
 import com.service.sport_companion.domain.model.type.FailedResultType;
 import com.service.sport_companion.domain.model.type.SuccessResultType;
-import com.service.sport_companion.domain.model.type.UserRole;
 import jakarta.transaction.Transactional;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -24,13 +21,10 @@ import org.springframework.stereotype.Service;
 public class VoteServiceImpl implements VoteService {
 
   private final VoteHandler voteHandler;
-  private final UserHandler userHandler;
   private final CandidateHandler candidateHandler;
 
   @Override
   public ResultResponse<Void> createVote(Long userId, CreateVoteDto voteDto) {
-    checkAdminRole(userId);
-
     // 동일한 날짜에 이미 등록돼있으면 추가로 등록할 수 없음
     if (voteHandler.existByStartDate(voteDto.getStartDate())) {
       throw new GlobalException(FailedResultType.ALREADY_EXISTS_VOTE_DATE);
@@ -61,8 +55,6 @@ public class VoteServiceImpl implements VoteService {
 
   @Override
   public ResultResponse<Void> updateVote(Long userId, Long voteId, CreateVoteDto voteDto) {
-    checkAdminRole(userId);
-
     // 투표 주제 업데이트
     VoteEntity voteEntity = voteHandler.getVoteEntity(voteId);
     voteEntity.update(voteDto);
@@ -87,18 +79,9 @@ public class VoteServiceImpl implements VoteService {
 
   @Override
   public ResultResponse<Void> deleteVote(Long userId, Long voteId) {
-    checkAdminRole(userId);
-
     candidateHandler.deleteVoteByVoteId(voteId);
     voteHandler.deleteVoteById(voteId);
 
     return ResultResponse.of(SuccessResultType.SUCCESS_DELETE_VOTE);
-  }
-
-  private void checkAdminRole(Long userId) {
-    UsersEntity usersEntity = userHandler.findByUserId(userId);
-    if (!usersEntity.getRole().equals(UserRole.ADMIN)) {
-      throw new GlobalException(FailedResultType.REQUIRED_ADMIN_ROLE);
-    }
   }
 }
