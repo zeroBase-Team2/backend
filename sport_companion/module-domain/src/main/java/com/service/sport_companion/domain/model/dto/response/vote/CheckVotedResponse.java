890f9d8fe61822192fc0d2b38ffb67cc4fadf370
yeon <p-yo00@naver.com>
2024-12-03 22:41:53 +0900

    Feat : 당일 투표 여부 응답 dto 생성

---
+++ sport_companion/module-domain/src/main/java/com/service/sport_companion/domain/model/dto/response/vote/CheckVotedResponse.java
@@ -0,0 +1,11 @@
+package com.service.sport_companion.domain.model.dto.response.vote;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class CheckVotedResponse {
+
+  private Boolean voted;
+}
