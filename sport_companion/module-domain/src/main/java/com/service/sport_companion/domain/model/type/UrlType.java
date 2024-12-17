1cde1855b4971c80625dd4f6241662978d33d74b
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-12-17 19:02:51 +0900

    Refactor : URL Type 수정

--- sport_companion/module-domain/src/main/java/com/service/sport_companion/domain/model/type/UrlType.java
+++ sport_companion/module-domain/src/main/java/com/service/sport_companion/domain/model/type/UrlType.java
@@ -8,9 +8,8 @@ public enum UrlType {
   KAKAO_TOKEN_URL("https://kauth.kakao.com/oauth/token"),
   KAKAO_USER_INFO_URL("https://kapi.kakao.com/v2/user/me"),
   RANDOM_NICKNAME_URL("https://www.rivestsoft.com/nickname/getRandomNickname.ajax"),
-  SIGNUP_URL("http://localhost:3000/signUp"),
   FRONT_LOCAL_URL("http://localhost:3000"),
-  FRONT_VERCEL_URL("https://front-end-jet-psi.vercel.app"),
+  FRONT_VERCEL_URL("https://front-end-choseongjus-projects-09f13a00.vercel.app"),
   MBC_NEWS_URL("https://imnews.imbc.com"),
   KAKAO_UNLINK_URL("https://kapi.kakao.com/v1/user/unlink"),
   ;
